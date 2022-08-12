b9da7e2fa72f8a894ae1568a6ee32b082a5ad972
sareenak <43492259+sareenak@users.noreply.github.com>
2022-08-12 14:25:19 +0400

    set properties in schema

--- models/User.js
+++ models/User.js
@@ -2,5 +2,35 @@ import mongoose from "mongoose";
 
 //setup schema
 const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please provide valid name'],
+        minlength:3,
+        maxlength:20,
+        trim:true
+},
+email:{
+    type:String,
+    required:[true,'Please provide valid email'],
+    unique:true,
+},
+password:{
+    type:String,
+    required:[true,'Please provide valid password'],
+    minlength:6,
     
+},
+lastName:{
+    type:String,
+    maxlength:20,
+    trim:true,
+    default:'lastName',
+},
+location:{
+    type:String,
+    maxlength:20,
+    trim:true,
+    default:'My city'
+},
+
 })
