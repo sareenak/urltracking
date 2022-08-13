da285cf0400c74eccfe8b7b497ac867247c8a45a
sareenak <43492259+sareenak@users.noreply.github.com>
2022-08-13 06:18:58 +0400

    Changed 201 to StatusCodes.CREATED

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ import { StatusCodes } from "http-status-codes"
 const register =async(req,res)=>{
 
     const user=await User.create(req.body)
-    res.status(201).json({user})
+    res.status(StatusCodes.CREATED).json({user})
 
 }
 const login =async(req,res)=>{
