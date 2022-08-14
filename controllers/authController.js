1665fa179072ab3cacf6e745a07da3ddfd2490de
sareenak <43492259+sareenak@users.noreply.github.com>
2022-08-14 16:30:29 +0400

    checked for duplicate email in controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,18 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
-class CustomAPIError extends Error{
-    constructor(message){
-        super(message)
-        }
-}
-class BadRequest extends CustomAPIError {
-    constructor(message){
-        super(message)
-        this.statusCode= StatusCodes.BAD_REQUEST
-    }
-}
-class NotFoundError extends CustomAPIError {
-    constructor(message){
-        super(message)
-        this.statusCode= StatusCodes.NOT_FOUND
-    }
-}
+import {BadRequestError} from "../errors/index.js"
+
+
 const register =async(req,res)=>{
     const {name,email,password}=req.body
 
     if(!name ||  !email || !password ){
-        throw new BadRequest('Please provide all values')
+        throw new BadRequestError('Please provide all values')
     }
-
+    const userAlreadyExist =await User.findOne({email})
+    if(userAlreadyExist){
+        throw new BadRequestError('Email already in use')
+    }
     const user=await User.create({name,email,password})
     res.status(StatusCodes.CREATED).json({user})
 
