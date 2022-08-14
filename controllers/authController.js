a3ffb75a6172996ea127fe19e3ec95b31f6b25fc
sareenak <43492259+sareenak@users.noreply.github.com>
2022-08-14 14:48:00 +0400

    BAD_Request and NOT_FOUND Error set

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,25 @@ import { StatusCodes } from "http-status-codes"
 class CustomAPIError extends Error{
     constructor(message){
         super(message)
+        }
+}
+class BadRequest extends CustomAPIError {
+    constructor(message){
+        super(message)
         this.statusCode= StatusCodes.BAD_REQUEST
     }
 }
-
+class NotFoundError extends CustomAPIError {
+    constructor(message){
+        super(message)
+        this.statusCode= StatusCodes.NOT_FOUND
+    }
+}
 const register =async(req,res)=>{
     const {name,email,password}=req.body
 
     if(!name ||  !email || !password ){
-        throw new CustomAPIError('Please provide all values')
+        throw new BadRequest('Please provide all values')
     }
 
     const user=await User.create({name,email,password})
