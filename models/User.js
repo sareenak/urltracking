d274056c608dc8dba88d6b591dd92e7b9086941b
sareenak <43492259+sareenak@users.noreply.github.com>
2022-08-15 11:23:53 +0400

    JWT_SECRET AND JWT_LIFETIME set in User.js

--- models/User.js
+++ models/User.js
@@ -46,7 +46,7 @@ UserSchema.pre('save',async function(){
    this.password=await bcrypt.hash(this.password,salt)
 })
 UserSchema.methods.createJWT=function(){
-    return jwt.sign({id:this._id},'jwtSecret' ,{expiresIn:'1d'})
+    return jwt.sign({id:this._id},process.env.JWT_SECRET ,{expiresIn:process.env.JWT_LIFETIME})
     //console.log(this)
 }
 export default mongoose.model('User',UserSchema)
