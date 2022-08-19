fe271a0cbd08940c49a4ddb61e8f419de8af61fc
sareenak <43492259+sareenak@users.noreply.github.com>
2022-08-19 11:26:52 +0400

    UnauthenticatedError setup and compare password setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
-import {BadRequestError} from "../errors/index.js"
+import {BadRequestError, UnauthenticatedError} from "../errors/index.js"
 
 
 const register =async(req,res)=>{
@@ -29,9 +29,17 @@ const login =async(req,res)=>{
     if(!email || !password ){
         throw new BadRequestError('Please provide all values')
     }
-    
-
-    res.send('login user')
+    const user=await User.findOne({email}).select('+password')
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const isPasswordCorrect=await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+     const token=user.createJWT
+     user.password=undefined
+    res.status(StatusCodes.OK).json({user,token,location:user.location})
 }
 const updateUser =async(req,res)=>{
     res.send('update user')
--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -5,7 +5,7 @@ import CustomAPIError from "./custom-api.js"
 class UnauthenticatedError extends CustomAPIError {
     constructor(message){
         super(message)
-        this.statusCode= StatusCodes.BAD_REQUEST
+        this.statusCode= StatusCodes.UNAUTHORIZED
     }
 }
 export default UnauthenticatedError
