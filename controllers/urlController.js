97b4b8f72c8dccc23203b21554fd276ff635f270
sareenak <43492259+sareenak@users.noreply.github.com>
2022-12-06 14:15:08 +0400

    displayed the urls of each user

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,21 +1,40 @@
 import Url from "../models/Url.js"
 import { StatusCodes } from "http-status-codes"
 import {BadRequestError,NotFoundError} from '../errors/index.js'
+import { urlencoded } from "express"
 
 const createUrl=async(req,res)=>{
    
-    const {website_url,campaign_source,campaign_medium,campaign_name}=req.body
+    const {websiteUrl,campaignSource,campaignMedium,campaignName}=req.body
    
-   if(!website_url || !campaign_source || !campaign_medium || !campaign_name){
+   if(!websiteUrl || !campaignSource || !campaignMedium || !campaignName){
    
     throw new BadRequestError('Please provide all values')
 
 }
+//generate url_uid
+
+const generateUid=async()=>{
+ let uid=Math.random().toString(36).slice(-6)
+ req.body.url_uid=uid
+ let uidExist= await Url.exists({url_uid:uid})
+ if(uidExist ){
+    return generateUid()
+ }
+
+ return uid
+}
+generateUid()
 
 req.body.createdBy=req.user.userId
 
 const url =await Url.create(req.body)
+// Url.updateMany({}, {
+//     $inc: { visitersCount: 1 }
+//   }).exec()
+await Url.findByIdAndUpdate(url._id,{$inc:{visitersCount:1}}).exec()
 res.status(StatusCodes.CREATED).json({url})
+
 
 
 }
@@ -23,7 +42,9 @@ const deleteUrl=async(req,res)=>{
     res.send('deleteUrl')
 }
 const getAllUrls=async(req,res)=>{
-    res.send('getAllUrls')
+    const urls=await Url.find({createdBy:req.user.userId})
+
+    res.status(StatusCodes.OK).json({urls,totalUrls: urls.length ,numberOfPages:1})
 }
 const updateUrl=async(req,res)=>{
     res.send('updateUrl')
--- models/Url.js
+++ models/Url.js
@@ -1,35 +1,50 @@
 import mongoose from "mongoose";
+import validator from "validator";
 //setup schema
 const UrlSchema=new mongoose.Schema({
-    website_url:{
+    websiteUrl:{
         type:String,
-        required:[true,'Please provide valid url'],
-        maxlength:100,
+        required:[true,'Please provide valid URL'],
+        validate:{validator: value=>validator.isURL(value,{protocols:['http','https','ftp'],require_tld:true,require_protocol:true}),
+        message:'Please provide valid URL'},
+        
+        
         
 },
-campaign_source:{
+campaignSource:{
     type:String,
     required:[true,'Please provide valid Campaign Source'],
     maxlength:50
 },
-campaign_medium:{
+campaignMedium:{
     type:String,
     required:[true,'Please Provide Campaign Medium ']
     
 },
-campaign_term:{
+campaignTerm:{
     type:String,
     default:""
     
 },
-campaign_content:{
+campaignContent:{
     type:String,
     default:''
 },
-campaign_name:{
+campaignName:{
     type:String,
     required:[true,'Please Provide Campaign Name']
 },
+url_uid:{
+      type:String,
+      unique:true,
+      
+},
+visitersCount:{
+    type:Number,
+    default:0,
+},
+
+
 createdBy:{
     type:mongoose.Types.ObjectId,
     ref:'User',
